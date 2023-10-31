6f9d67bf2f147621be527337d8b746611c3f1374
Keith Vassallo <keith@icemalta.com>
2023-10-31 15:23:47 +0000

    Starter complete.

---
+++ aboutphp.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<body>
+
+    <div class="container">
+        <h1>PHP</h1>
+        <p>
+            PHP is a general-purpose scripting language geared towards web development.
+            It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1993 and released in 1995.
+            The PHP reference implementation is now produced by the PHP Group.
+            PHP was originally an abbreviation of Personal Home Page, but it now stands for the recursive initialism
+            PHP: Hypertext Preprocessor. 
+        </p>
+    </div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+
+<body>
+
+    <div class="container">
+        <h1>Learning PHP</h1>
+        <p>Welcome to the <strong>Learning PHP</strong> home page!</p>
+        <p>Select a link above to start learning. about a language with years of heritage!</p>
+    </div>
+    
+</body>
+
+</html>
